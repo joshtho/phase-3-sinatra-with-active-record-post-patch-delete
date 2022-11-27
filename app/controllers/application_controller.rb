3b7419a263f735b1a0d13c905edd4014485295bd
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-11-27 15:47:45 -0600

    commit tree 1fe0469c20ee2838d360b5e14fdddeeb4dcefd3b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
+
+require 'pry'
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-
+  
   get '/games' do
     games = Game.all.order(:title).limit(10)
     games.to_json
   end
-
+  
   get '/games/:id' do
     game = Game.find(params[:id])
 
@@ -16,4 +18,29 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  delete '/reviews/:id' do 
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+  
+  post '/reviews' do 
+    review = Review.create(
+    score: params[:score],
+    comment: params[:comment],
+    game_id: params[:game_id],
+    user_id: params[:user_id]
+  )
+  review.to_json
+  end
+
+  patch '/reviews/:id' do 
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+  
 end
